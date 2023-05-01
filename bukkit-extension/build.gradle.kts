a27281e1392de507cefecf330079e63e7f8a6bc0
TheMeinerLP <p.glanz@madfix.me>
2023-05-01 16:05:05 +0200

    Implement to publish bukkit extension

--- bukkit-extension/build.gradle.kts
+++ bukkit-extension/build.gradle.kts
@@ -1,8 +1,29 @@
+import org.ajoberstar.grgit.Grgit
+import java.util.*
+
 plugins {
     kotlin("jvm") version "1.8.20"
+    `java-library`
+    `maven-publish`
+    signing
+    id("org.jetbrains.dokka") version "1.8.10"
 }
 
 group = "dev.themeinerlp"
+var baseVersion by extra("1.0.0")
+var extension by extra("")
+var snapshot by extra("-SNAPSHOT")
+
+ext {
+    val git: Grgit = Grgit.open {
+        dir = File("$rootDir/.git")
+    }
+    val revision = git.head().abbreviatedId
+    extension = "%s+%s".format(Locale.ROOT, snapshot, revision)
+}
+
+
+version = "%s%s".format(Locale.ROOT, baseVersion, extension)
 
 repositories {
     mavenCentral()
@@ -30,4 +51,79 @@ kotlin {
             languageVersion = "2.0"
         }
     }
+}
+
+val sourceJar by tasks.register<Jar>("kotlinJar") {
+    from(sourceSets.main.get().allSource)
+    archiveClassifier.set("sources")
+}
+val dokkaJavadocJar by tasks.register<Jar>("dokkaHtmlJar") {
+    dependsOn(rootProject.tasks.dokkaHtml)
+    from(rootProject.tasks.dokkaHtml.flatMap { it.outputDirectory })
+    archiveClassifier.set("html-docs")
+}
+
+val dokkaHtmlJar by tasks.register<Jar>("dokkaJavadocJar") {
+    dependsOn(rootProject.tasks.dokkaJavadoc)
+    from(rootProject.tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components.findByName("java"))
+            groupId = "dev.themeinerlp.plugin-debug"
+            artifactId = "bukkit-extension"
+            version = "%s%s".format(Locale.ROOT, baseVersion, snapshot)
+            artifact(dokkaJavadocJar)
+            artifact(dokkaHtmlJar)
+            artifact(sourceJar)
+            pom {
+                name.set("Bukkit Extension")
+                description.set("The extension for bukkit/paper/spigot fir plugin debug")
+                url.set("https://github.com/OneLiteFeatherNET/Plugin-Debug")
+                licenses {
+                    license {
+                        name.set("AGPL-3.0")
+                        url.set("https://github.com/OneLiteFeatherNET/Plugin-Debug/blob/main/LICENSE")
+                    }
+                }
+                issueManagement {
+                    system.set("Github")
+                    url.set("https://github.com/OneLiteFeatherNET/Plugin-Debug/issues")
+                }
+                developers {
+                    developer {
+                        id.set("TheMeinerLP")
+                        name.set("Phillipp Glanz")
+                        email.set("p.glanz@madfix.me")
+                    }
+                }
+                scm {
+                    connection.set("scm:git@github.com:OneLiteFeatherNET/Plugin-Debug.git")
+                    developerConnection.set("scm:git@github.com:OneLiteFeatherNET/Plugin-Debug.git")
+                    url.set("https://github.com/OneLiteFeatherNET/Plugin-Debug")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = if (version.toString().contains("SNAPSHOT")) uri(snapshotsRepoUrl) else uri(releasesRepoUrl)
+            credentials {
+                username = System.getenv("OSSRH_USERNAME")
+                password = System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications)
 }
