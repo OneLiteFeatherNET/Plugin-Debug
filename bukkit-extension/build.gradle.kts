585b1b1d0c9e3a9724c25d8883d89deae9e8299b
TheMeinerLP <p.glanz@madfix.me>
2023-05-01 14:36:10 +0200

    Add gradle module

---
+++ bukkit-extension/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+}
+
+group = "net.onelitefeather"
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    implementation(rootProject)
+    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+    implementation("io.papermc:paperlib:1.0.8")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+    sourceSets.all {
+        languageSettings {
+            languageVersion = "2.0"
+        }
+    }
+}
