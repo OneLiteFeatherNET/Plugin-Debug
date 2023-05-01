ed9bae6ed528133359a9332627e3a3f88ab1033f
TheMeinerLP <p.glanz@madfix.me>
2023-05-01 13:05:36 +0200

    Improve build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,38 @@
+import org.ajoberstar.grgit.Grgit
+import java.util.*
+
 plugins {
     kotlin("jvm") version "1.8.20"
+    id("org.ajoberstar.grgit") version "5.2.0"
     `java-library`
 }
 
+if (!File("$rootDir/.git").exists()) {
+    logger.lifecycle(
+        """
+    **************************************************************************************
+    You need to fork and clone this repository! Don't download a .zip file.
+    If you need assistance, consult the GitHub docs: https://docs.github.com/get-started/quickstart/fork-a-repo
+    **************************************************************************************
+    """.trimIndent()
+    ).also { System.exit(1) }
+}
+
 group = "net.onelitefeather"
-version = "1.0.0-SNAPSHOT"
+var baseVersion by extra("1.0.0")
+var extension by extra("")
+var snapshot by extra("-SNAPSHOT")
+
+ext {
+    val git: Grgit = Grgit.open {
+        dir = File("$rootDir/.git")
+    }
+    val revision = git.head().abbreviatedId
+    extension = "%s+%s".format(Locale.ROOT, snapshot, revision)
+}
+
+
+version = "%s%s".format(Locale.ROOT, baseVersion, extension)
 
 repositories {
     mavenCentral()
@@ -14,10 +42,17 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    test {
+        useJUnitPlatform()
+    }
 }
 
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(17)
+    sourceSets.all {
+        languageSettings {
+            languageVersion = "2.0"
+        }
+    }
 }
