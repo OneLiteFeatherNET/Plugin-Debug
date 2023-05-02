c187c0f18e4c622b902026fae3c3bd9741d19cd6
TheMeinerLP <p.glanz@madfix.me>
2023-05-02 12:12:23 +0200

    Go back to snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ if (!File("$rootDir/.git").exists()) {
 group = "dev.themeinerlp"
 var baseVersion by extra("1.0.0")
 var extension by extra("")
-var snapshot by extra("")
+var snapshot by extra("-SNAPSHOT")
 
 ext {
     val git: Grgit = Grgit.open {
