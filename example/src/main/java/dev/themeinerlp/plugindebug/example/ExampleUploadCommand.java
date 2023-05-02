744943f6b8050b788c44a3d1b6871036f352aa5a
TheMeinerLP <p.glanz@madfix.me>
2023-05-02 18:31:36 +0200

    Improve working example

--- example/src/main/java/dev/themeinerlp/plugindebug/example/ExampleUploadCommand.java
+++ example/src/main/java/dev/themeinerlp/plugindebug/example/ExampleUploadCommand.java
@@ -7,7 +7,6 @@ import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
