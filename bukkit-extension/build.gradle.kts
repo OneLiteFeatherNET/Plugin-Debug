6df372d137f576cad118f8344d2ca6f357b45009
OneLiteFeather <seelenretterin@onelitefeather.net>
2025-05-30 21:19:41 +0200

    Fix repository URL for PaperMC in build.gradle.kts

--- bukkit-extension/build.gradle.kts
+++ bukkit-extension/build.gradle.kts
@@ -26,7 +26,7 @@ version = "%s%s".format(Locale.ROOT, baseVersion, extension)
 
 repositories {
     mavenCentral()
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
